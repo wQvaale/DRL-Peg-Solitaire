bb494e36263c31f30232f0b9d4ab7feb38c78d3c
Toralf Frich <toralffrich@gmail.com>
2021-01-26 09:23:04 +0100

    added nodes to graph

--- client/Viz.py
+++ client/Viz.py
@@ -5,18 +5,29 @@ size = 5
 t = Triangle(size, [(4,2)])
 t.initialize_neighbours()
 
+
+
 def initialise_graph(hexgrid):
     G = nx.Graph()
     alphabet = "abcdefghijklmnopqrstuvwxyz"
     neighbours = hexgrid.get_neighbours()
+    print(hexgrid.grid)
+
+    """Add nodes to graph"""
+    for i in range(len(t.grid)):
+        for j in range(len(t.grid[i])):
+            G.add_node(t.grid[i][j].__str__())
+
+    """Add edges to graph"""
     for i in range(len(neighbours)):
         node_edges = []
         for j in range(len(neighbours[i])):
             node_edges.append((alphabet[i], neighbours[i][j]))
         G.add_edges_from(node_edges)
-        print(node_edges)
 
-initialise_graph(t)
+
+
+#initialise_graph(t)
 
 
 
