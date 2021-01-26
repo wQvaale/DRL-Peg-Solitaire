e4e802c549ea07b44fb6e12716abf2c1fe1e675f
Toralf Frich <toralffrich@gmail.com>
2021-01-26 09:52:17 +0100

    first drawing

--- client/Viz.py
+++ client/Viz.py
@@ -1,17 +1,17 @@
 import networkx as nx
+import matplotlib.pyplot as plt
+from networkx.drawing.nx_agraph import graphviz_layout, to_agraph
+import pygraphviz as pgv
 from HexGrid import *
 
 size = 5
 t = Triangle(size, [(4,2)])
 t.initialize_neighbours()
 
-
-
 def initialise_graph(hexgrid):
     G = nx.Graph()
     alphabet = "abcdefghijklmnopqrstuvwxyz"
     neighbours = hexgrid.get_neighbours()
-    print(hexgrid.grid)
 
     """Add nodes to graph"""
     for i in range(len(t.grid)):
@@ -25,9 +25,18 @@ def initialise_graph(hexgrid):
             node_edges.append((alphabet[i], neighbours[i][j]))
         G.add_edges_from(node_edges)
 
+    return G
+
+G = initialise_graph(t)
+
+print(G.nodes())
+print(G.edges())
 
 
-#initialise_graph(t)
+nx.draw(G)
+plt.show()
+
+
 
 
 
