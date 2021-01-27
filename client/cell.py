064e8b3a8dcf68171660468ee69394238842f7c9
Toralf Frich <toralffrich@gmail.com>
2021-01-27 11:00:26 +0100

    added cell from main

--- client/cell.py
+++ client/cell.py
@@ -1,11 +1,13 @@
 class Cell:
 
-    def __init__(self, cell_id=None, neighbours=[], empty=False):
+    def __init__(self, x, y, cell_id=None, neighbours=[], empty=False):
         self.cell_id = cell_id
+        self.x = x
+        self.y = y 
         self.neighbours = neighbours
         self.empty = empty
 
         
     def __str__(self):
-        return self.cell_id
+        return str(self.cell_id)
         
