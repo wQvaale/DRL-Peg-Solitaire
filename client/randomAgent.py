a4864cdf9776c9d21418396ba1a0bb40d37d5a0c
Tjedor <theo.aw@hotmail.com>
2021-01-28 11:28:36 +0100

    fix

--- client/randomAgent.py
+++ client/randomAgent.py
@@ -3,10 +3,9 @@ import random
 
 class RandomAgent:
 
-    def getMove(self, board):
-        jumper = random.randint(0, board.size*(board.size+1)/2 - 1)
-        jumpee = random.randint(0, board.size*(board.size+1)/2 - 1)
-        return jumper, jumpee
+    def getMove(self, moves):
+       
+        return random.choice(moves)
 
 
         
