ce2dbc4a310a82e280e4d139816d99c2d2952011
Tjedor <theo.aw@hotmail.com>
2021-02-09 18:43:13 +0100

    dl

--- agent.py
+++ agent.py
@@ -1,6 +1,7 @@
 import random
 from config import Config
 from collections import defaultdict
+import torch
 
 
 class RandomAgent:
@@ -14,6 +15,53 @@ class Critic:
     def __init__(self):
         self.state_value = defaultdict(lambda: 0)
         self.eligibility = defaultdict(lambda: 0)
+
+
+class NeuralCritic(torch.nn.Module):
+    def __init__(self, layers=4, sizes=[15, 8, 1]):
+        super().__init__()
+        self.model = torch.nn.Sequential()
+        self.layers = []
+        self.eligibility = [torch.zeros(param.shape) for param in self.parameters()]
+
+        for i in range(len(sizes) - 1):
+            layer = torch.nn.Linear(sizes[i], sizes[i + 1])
+            self.model.add_module(f"{i}", layer)
+
+    def forward(self, X):
+        self.z = X
+        for layer in self.model:
+            self.z = layer(self.z)
+            self.z = self.sigmoid(self.z)
+        return self.z
+
+    @staticmethod
+    def sigmoid(s):
+        return 1 / (1 + torch.exp(-s))
+
+    @staticmethod
+    def sigmoidPrime(s):
+        # derivative of sigmoid
+        return s * (1 - s)
+
+    def backward(self, X, y, o):
+        self.o_error = y - o  # error in output
+        self.o_delta = self.o_error * self.sigmoidPrime(o)  # derivative of sig to error
+        self.z2_error = torch.matmul(self.o_delta, torch.t(self.W2))
+        self.z2_delta = self.z2_error * self.sigmoidPrime(self.z2)
+        self.W1 += torch.matmul(torch.t(X), self.z2_delta)
+        self.W2 += torch.matmul(torch.t(self.z2), self.o_delta)
+
+    def train(self, X, y):
+        # forward + backward pass for training
+        o = self.forward(X)
+        self.backward(X, y, o)
+
+    def saveWeights(self, model):
+        # we will use the PyTorch internal storage functions
+        torch.save(model, "NN")
+        # you can reload model with all the weights and so forth with:
+        # torch.load("NN")
 
 
 class Actor:
@@ -51,7 +99,8 @@ class ActorCriticAgent:
                                                                  (state, action)] + self.cfg.learning_rate * gamma * \
                                                              self.actor.eligibility[(state, action)]
 
-            self.actor.eligibility[(state, action)] = self.cfg.discount * self.cfg.decay * self.actor.eligibility[(state, action)]
+            self.actor.eligibility[(state, action)] = self.cfg.discount * self.cfg.decay * self.actor.eligibility[
+                (state, action)]
 
     def get_move(self, state, moves, e_greedy=0.3, choose_best=False):
         best = None
@@ -70,3 +119,76 @@ class ActorCriticAgent:
     def set_eligibility(self, state, action):
         self.actor.eligibility[(state, action.stringify())] = 1
         self.critic.eligibility[state] = 1
+
+
+class NeuralAgent:
+    def __init__(self, cfg: Config):
+        self.critic = NeuralCritic()
+        self.actor = Actor()
+        self.episode = []
+        self.wins = 0
+        self.cfg = cfg
+
+    def flush(self):
+        self.actor.eligibility = defaultdict(lambda: 0)
+        self.critic.eligibility = [torch.zeros(param.shape) for param in self.critic.parameters()]
+        self.episode = []
+
+    def update(self, current_state, action, reward, new_state):
+        self.episode.append((current_state, action.stringify()))
+        print("Inne i UPDATE til NeuralAgent")
+        self.set_eligibility(current_state, action)
+
+        current_state_tensor = torch.Tensor([int(b) for b in current_state])
+        new_state_tensor = torch.Tensor([int(b) for b in new_state])
+
+        gamma = reward + self.cfg.discount * (self.critic(new_state_tensor) - self.critic(current_state_tensor))
+
+        for (state, action) in self.episode:
+            print("Inne i loopen til update")
+            self.update_weights(state, gamma)
+
+            # Update eligibility
+            for i in range(len(self.critic.eligibility)):
+                self.critic.eligibility[i] *= self.cfg.discount * self.cfg.decay
+
+            self.actor.state_action_pairs[(state, action)] = self.actor.state_action_pairs[
+                                                                 (state, action)] + self.cfg.learning_rate * gamma * \
+                                                             self.actor.eligibility[(state, action)]
+
+            self.actor.eligibility[(state, action)] = self.cfg.discount * self.cfg.decay * self.actor.eligibility[
+                (state, action)]
+
+    def update_weights(self, state, gamma):
+        # Set gradients to zero
+        self.critic.zero_grad()
+
+        tensor_state = torch.Tensor([int(b) for b in state])
+        pred_val = self.critic(tensor_state)
+        pred_val.backward()
+        # TODO: @wQuole look into logs
+
+        for i, weight in enumerate(self.critic.parameters()):
+            self.critic.eligibility[i] += weight.grad
+            with torch.no_grad():
+                print("self.critic.model[i]", self.critic.model[i])
+                self.critic.model[i].weight.add_(self.cfg.learning_rate * gamma * self.critic.eligibility[i])
+
+    def get_move(self, state, moves, e_greedy=0.3, choose_best=False):
+        best = None
+
+        for action in moves:
+            print("Inne i get_move")
+            a = action.stringify()
+            if best is None or best[2] < self.actor.state_action_pairs[(state, a)]:
+                best = (state, action, self.actor.state_action_pairs[(state, a)])
+
+        if random.random() < e_greedy or choose_best:
+            m = best[1]
+        else:
+            m = random.choice(moves)
+        return m
+
+    def set_eligibility(self, state, action):
+        self.actor.eligibility[(state, action.stringify())] = 1
+        self.critic.eligibility = [torch.ones(param.shape) for param in self.critic.parameters()]
--- main.py
+++ main.py
@@ -18,14 +18,14 @@ def train_and_test_triangle(agent):
                 if i % 100 == 0:
                     print(i)
 
-                sim_world = SimWorld(shape="Triangle", size=4, holes=[(row, col)])
+                sim_world = SimWorld(shape="Triangle", size=5, holes=[(row, col)])
                 sim_world.play_RL(agent, 0.5)
                 agent.flush()
     agent_wins = agent.wins
 
     for row in range(0, 5):
         for col in range(row):
-            sim_world = SimWorld(shape="Triangle", size=4, holes=[(row, col)])
+            sim_world = SimWorld(shape="Triangle", size=5, holes=[(row, col)])
             sim_world.play_RL(agent, greed=0, vis=True, choose_best=True)
             agent.flush()
 
@@ -37,14 +37,14 @@ def train_diamond(agent, epochs=1000):
     holes = [(1,1), (1,2), (2,1), (2,2)]
     for hole in holes:
         for i in range(epochs):
-            sim_world = SimWorld(shape="Diamond", size=4, holes=[hole])
+            sim_world = SimWorld(shape="Diamond", size=8, holes=[hole])
             sim_world.play_RL(agent, 0.5)
             agent.flush()
 
     trained_agent_wins = agent.wins
 
     for hole in holes:
-        sim_world = SimWorld(shape="Diamond", size=4, holes=[hole], viz_toggle=True)
+        sim_world = SimWorld(shape="Diamond", size=8, holes=[hole], viz_toggle=True)
         sim_world.play_RL(agent, greed=0, vis=True, choose_best=True)
         agent.flush()
 
@@ -61,5 +61,11 @@ if __name__ == '__main__':
     #sim_world = SimWorld(shape="Diamond", viz_toggle=1)
     #sim_world.play_solitaire_random_agent()
 
-    agent = ActorCriticAgent(cfg)
-    train_diamond(agent)
+    agent = NeuralAgent(cfg)
+    train_and_test_triangle(agent)
+
+    # critic = NeuralCritic()
+    # data = list(range(16))
+    # X = torch.Tensor(data)
+
+
